e7729e7cc1297cabab442bb1e1310daa550a0a04
nime003 <nime003@student.kristiania.no>
2024-12-17 18:26:35 +0100

    written more on education

--- cv-side/src/app/career/page.tsx
+++ cv-side/src/app/career/page.tsx
@@ -39,8 +39,22 @@ export default function Career() {
             August 2021 — June 2024
           </Typography>
           <Divider sx={{ borderColor: 'rgba(255,255,255,0.3)', mb: 2 }} />
-          <Typography variant="body1" sx={{ lineHeight: 1.7, color: 'var(--color-text-light)' }}>
-            Earned a bachelor’s degree in programming, building a solid foundation in modern software development and problem-solving.
+          <Typography variant="body1" sx={{ lineHeight: 1.7, whiteSpace: 'pre-line', color: 'var(--color-text-light)' }}>
+            I have earned a bachelor’s degree in programming, this degree mostly entails different programming and IT courses.
+            I have gained valuable information and experience with different technologies and methodologies.
+            My classes were heavily focused on hands on programming, often with the goal of building a project from beginning to end. 
+            <br />
+            <br />
+            I have learned a couple of programming languages aswell. Java is the one i am the most familiar with, i've made several
+            programs using java as the foundation. I also have experience making a java backend for a website, and using java for 
+            a microservices backend. We also had front-end classes focusing on learning the basics of web-design. I have experience using 
+            HTML/CSS, React, Vue.js and Next.js.
+            <br />
+            I also have experience hosting both the backend, database and frontend on Microsoft Azure. Along with that we also had a class that utilized AWS along with github workflows, terraform and dockerization.
+            <br />
+            <br />
+            The rest of the languages i know are C#, arduino and python. I have done some game development and completet a software design class that included making a standalone programming
+            with C#. I also had an embedded systems class where i made a version of the game pong with analog joystick input.
           </Typography>
         </Paper>
 
@@ -72,8 +86,8 @@ export default function Career() {
           </Typography>
           <Divider sx={{ borderColor: 'rgba(255,255,255,0.3)', mb: 2 }} />
           <Typography variant="body1" sx={{ lineHeight: 1.7, color: 'var(--color-text-light)' }}>
-            Providing customer service, product guidance, and photography advice. 
-            Helping customers navigate printing solutions and ensuring a smooth, pleasant experience.
+            During my work at CEWE Japan Photo i'm responsible for providing customer service, product guidance, and photography advice. 
+            I am also responsible for the cash register, shelfing new product, and fulfilling online orders.
           </Typography>
         </Paper>
 
